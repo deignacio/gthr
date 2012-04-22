1f565b21c5edefc814f9230f8ae0dfc66aa02899
David Ignacio <deignacio@gmail.com>
2012-04-22 19:12:03 -0400

    add process_push tests

--- tests/test_gthr.py
+++ tests/test_gthr.py
@@ -1,3 +1,4 @@
+from mock import Mock
 import unittest
 
 from gthr import Gthr
@@ -40,3 +41,37 @@ class GthrTest(unittest.TestCase):
     def test_process_line_refid(self):
         self._test_process_line(' + e20548a...e2524c4 HEAD^ -> master (forced update)',
                                 {'src_ref': 'HEAD^'})
+
+    def _test_process_push(self, lines, expected):
+        p = Mock()
+        p.stdout.readline.side_effect = lambda: lines.pop() if lines else ""
+
+        self._create_gthr([])
+        info = self.gthr.process_push(p)
+        self.assertEquals(expected, info)
+
+    def test_process_push_none(self):
+        self._test_process_push(['hello world\n',
+                                'hi mom\n',
+                                'not matching\n'],
+                               {})
+
+    def test_process_push_launching(self):
+        self._test_process_push(['hello world\n',
+                                 '-----> Launching... done, v100\n',
+                                'hi mom\n'],
+                                {'version': '100'})
+
+    def test_process_push_refid(self):
+        self._test_process_push(['hello world\n',
+                                 ' + e20548a...e2524c4 HEAD^ -> master (forced update)\n',
+                                 'hi mom\n'],
+                                {'src_ref': 'HEAD^'})
+
+    def test_process_push_both(self):
+        self._test_process_push(['hello world\n',
+                                 '-----> Launching... done, v100\n',
+                                 ' + e20548a...e2524c4 HEAD^ -> master (forced update)\n',
+                                'hi mom\n'],
+                                {'version': '100',
+                                 'src_ref': 'HEAD^'})
